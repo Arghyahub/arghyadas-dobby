f3d3730aa98f8e1b21286f84d12bdece1fa138d4
Arghya <arghyadas242004@gmail.com>
2023-05-26 11:24:20 +0530

    Add: Frontend Logic almost done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,75 +1,116 @@
 // eslint-disable-next-line no-unused-vars
-import React , { useState , useEffect } from 'react' ;
-import {Link} from "react-router-dom" ;
-const backendURL = process.env.REACT_APP_BACKEND ;
+import React, { useState, useEffect , useRef } from 'react';
+import { Link } from "react-router-dom";
+const backendURL = process.env.REACT_APP_BACKEND;
 
 const Home = () => {
-  const [LoggedIn, setLoggedIn] = useState(false) ;
-  const [UploadClick, setUploadClick] = useState(false) ;
+  const inputRef = useRef(null);
+	const [LoggedIn, setLoggedIn] = useState(true);
+	const [UploadClick, setUploadClick] = useState(false);
+	const [Images, setImages] = useState([{ img: "https://picsum.photos/seed/picsum/500/300", name: "mountain" }]);
+	const [TempImg, setTempImg] = useState([]);
 
-	useEffect(() => {
-		const checkUser = async () => {
-			// Work to be done
+  const [Search, setSearch] = useState('') ;
+
+
+	const checkUser = async () => {
+		if (localStorage.getItem("token") === undefined || localStorage.getItem("token") === null) {
+			return;
 		}
-	},[])
-	
+		try {
+			const resp = await fetch(`${backendURL}/home`, {
+				method: "GET",
+				headers: {
+					"authorization": localStorage.getItem("token")
+				}
+			})
+			setLoggedIn(true);
+			const json = await resp.json();
+			setImages(json.images);
+		}
+		catch (err) {
+			setLoggedIn(false);
+			console.log(err);
+		}
+	}
+	useEffect(() => {
 
-  return (
-    <div className="flex-col hi-100">
 
-      <div id="navbar" className='fl-row border acen' style={{position: 'sticky' , top: '0' , backgroundColor: 'white'}}>
-        <div id="navl" className='fl-row acen'>
-          <img src="logo.png" alt="logo" />
-          The ImageStore
-        </div>
-        { LoggedIn && (
-          <div id="navr">
-            <button>logout</button>
-          </div>
-        )}
-      </div>
+		// Call the function
+	}, [])
 
-      {LoggedIn ? (
+	const handleImg = async (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const img = formData.get("image");
+		const name = formData.get("name");
+
+		try {
+			const resp = await fetch(`${backendURL}/postImg`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ img, name })
+			})
+			// Call check user function here
+		}
+		catch (err) {
+			console.log(err);
+		}
+	}
+
+  const handleSearch = () => {
+    const searchImg = Images.filter((objs) => {
+      return objs.name === Search;
+    }) ;
+    setTempImg([...Images]) ;
+    setImages(searchImg) ;
+  }
+
+
+	return (
+		<div className="flex-col hi-100">
+
+			<div id="navbar" className='fl-row border acen' style={{ position: 'sticky', top: '0', backgroundColor: 'white' }}>
+				<div id="navl" className='fl-row acen'>
+					<img src="logo.png" alt="logo" />
+					The ImageStore
+				</div>
+				{LoggedIn && (
+					<div id="navr">
+						<button>logout</button>
+					</div>
+				)}
+			</div>
+
+			{LoggedIn ? (
 				<>
 					<div id="uploadbtn" className="fl-col">
 						<button onClick={() => setUploadClick(!UploadClick)}>Upload</button>
 						{UploadClick && (
 							<div id="upload-form" className='border'>
-								<form className='fl-col'>
-								<label for="image">Image </label>
-								<input type="file" name="fileToUpload" id="fileToUpload" />
-								<input type="submit" value="Upload Image" name="submit" />
+								<form onSubmit={handleImg} className='fl-col'>
+									<label for="image">Image </label>
+									<input type="file" name="image" id="image" />
+									<label htmlFor="name">ImageName</label>
+									<input type="text" name='name' />
+									<input type="submit" value="Upload Image" name="submit" />
 								</form>
 							</div>
 						)}
 					</div>
+
+					<div className='fl-row'>
+						<button><img src="search.png" alt="search" /></button>
+						<input type="text" onChange={(event) => setSearch(event.target.value)} ref={inputRef} />
+            <button onClick={()=> { inputRef.current.value=""; setImages([...TempImg]) }}>Clear</button>
+					</div>
+
 					<div className="fl-row jcon-sar fwrap f1 personal-img">
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
-						<img src="https://picsum.photos/id/237/200/300" alt="nothing" />
+						{Images.map((Imge) => (
+							<img src={Imge.img} alt={Imge.name} key={Imge.name} />
+						))}
 					</div>
 				</>
 			) : (
@@ -78,8 +119,8 @@ const Home = () => {
 					<p>Login or Signin <Link to={'/'}>Here</Link></p>
 				</div>
 			)}
-    </div>
-  )
+		</div>
+	)
 }
 
 export default Home
